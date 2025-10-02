71ecd0dc3b7250692a3fbdc25226cbdd2608e4fd
Paul Stern <paulstern45@gmail.com>
2025-10-02 12:05:21 +0300

    (draft) add TaskTable

--- ui/task_table.go
+++ ui/task_table.go
@@ -6,3 +6,14 @@ type TaskTable struct {
 	*tview.Table
 	app *App
 }
+
+func NewTaskTable(app *App) *TaskTable {
+	return &TaskTable{
+		Table: tview.NewTable(),
+		app:   app,
+	}
+}
+
+func (t *TaskTable) Update() {
+
+}
